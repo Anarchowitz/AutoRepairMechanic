53ccbadc8be972d1d3b04c9bfc09ff446cc9a1b4
Anarch <123408756+Anarchowitz@users.noreply.github.com>
2023-01-25 17:52:06 +0300

    Add files via upload

---
+++ AutoRep.lua
@@ -0,0 +1,54 @@
+require "lib.moonloader"
+require "lib.samp.events"
+local sampev = require "lib.samp.events"
+local main_color = 0x698CC8
+
+function main()
+	if not isSampLoaded() or not isSampfuncsLoaded() then return end
+	while not isSampAvailable() do wait(100) end
+
+	sampAddChatMessage("Mechanic Helper Loaded", main_color)
+	
+	while true do
+		wait(0)
+		local closestid = getNearestDriver()
+		if isKeyJustPressed(0x71) and not sampIsCursorActive() then
+		sampSendChat("/repare " .. closestid .. " 500")
+		end
+		
+		local closestid = getNearestDriver()
+		if isKeyJustPressed(0x72) and not sampIsCursorActive() then
+		sampSendChat("/filscar " .. closestid .. " 260")
+		end
+    end
+
+	while true do
+		wait(0)
+	end
+end
+
+function sampev.onServerMessage(color, text)
+	lua_thread.create(function()
+		if text:find("Вы предложили .+ починить транспорт за") then
+			wait(30000)
+			sampAddChatMessage("Можно чинить!", main_color)
+		end
+	end)
+end
+
+function getNearestDriver()
+    local mx, my, mz = getCharCoordinates(PLAYER_PED)
+    local nearId, dist = -1, 999
+    for i, handle in ipairs(getAllChars()) do
+        if doesCharExist(handle) and handle ~= PLAYER_PED and isCharInAnyCar(handle) then
+            local vx, vy, vz = getCharCoordinates(handle)
+            local cDist = getDistanceBetweenCoords3d(mx, my, mz, vx, vy, vz)
+            local r, id = sampGetPlayerIdByCharHandle(handle)
+            if r and cDist < dist then
+                dist = cDist
+                nearId = id
+            end
+        end
+    end
+    return nearId
+end
